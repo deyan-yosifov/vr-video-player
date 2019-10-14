90e2a0825c56192feae76ceb936f12680d626e6e
deyan-yosifov <yosifov.deyan@gmail.com>
2019-10-14 16:04:02 +0300

    Upgrading to Unity 2019.2 and adding sample scene with initialized oculus user ID for the local avatar.

---
+++ VR Video Player/Assets/Scripts/WorkshopSampleScripts/OculusIdInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Oculus.Platform;
+using Oculus.Platform.Models;
+using UnityEngine;
+
+namespace WorkshopSampleScripts
+{
+    [RequireComponent(typeof(OvrAvatar))]
+    public class OculusIdInitializer : MonoBehaviour
+    {
+        private OvrAvatar localAvatar;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            this.localAvatar = this.GetComponent<OvrAvatar>();
+
+            if (this.localAvatar.ShowFirstPerson == true && this.localAvatar.ShowThirdPerson == false)
+            {
+                Core.Initialize();
+                Users.GetLoggedInUser().OnComplete(this.GetLoggedInUserCallback);
+                Request.RunCallbacks();  //avoids race condition with OvrAvatar.cs Start().
+            }
+            else
+            {
+                Debug.LogError("Local avatar should be shown first person and not third person.");
+            }
+        }
+
+        private void GetLoggedInUserCallback(Message<User> message)
+        {
+            if (!message.IsError)
+            {
+                this.localAvatar.oculusUserID = message.Data.ID.ToString();
+                Debug.Log($"Local avatar ID: {this.localAvatar.oculusUserID}");
+            }
+        }
+    }
+}
